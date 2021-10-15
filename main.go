07729fb0e485fc2a995878852e06515fa3fcfe24
tansoz <731055667@qq.com>
2021-10-15 14:55:26 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type FlightRecord struct {
+	CreatedAt       string `json:"create_at"`
+	CreatedStr      string `json:"create_str"`
+	FlightMissionId string `json:"flight_mission_id"`
+	Id              string `json:"id"`
+	LocalId         string `json:"local_id"`
+	Message         string `json:"message"`
+	RecordAt        string `json:"record_at"`
+}
+
+func main() {
+	data := ParseData(ReadFile("test.json"))
+	max := int64(-1)
+	min := int64(-1)
+	for _, v := range data {
+		tmp := ParseTimestamp(v.CreatedStr)
+		if max == -1 {
+			max = tmp
+		} else if max < tmp {
+			max = tmp
+		}
+		if min == -1 {
+			min = tmp
+		} else if min > tmp {
+			min = tmp
+		}
+	}
+	fmt.Printf("%0.2f minutes\n", float64(max-min)/60000000000.0)
+}
+func ParseTimestamp(timestamp string) int64 {
+
+	if strings.Count(timestamp, ":") > 2 {
+		timestamp = regexp.MustCompile(":(\\d+)$").ReplaceAllString(timestamp, ".$1")
+	}
+
+	t, err := time.Parse("2006-01-02 15:04:05", timestamp)
+	if err != nil {
+		panic(err)
+	}
+	return t.UnixNano()
+}
+func ReadFile(filepath string) []byte {
+	var tmp bytes.Buffer
+	fp, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+	tmp.ReadFrom(fp)
+	return tmp.Bytes()
+}
+func ParseData(data []byte) (frs []FlightRecord) {
+	err := json.NewDecoder(bytes.NewReader(data)).Decode(&frs)
+	if err != nil {
+		panic(err)
+	}
+	return frs
+}
