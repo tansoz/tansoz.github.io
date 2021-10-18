faaaa894800a28eeece8afcdf0957efa46faab49
tansoz <731055667@qq.com>
2021-10-18 08:24:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,20 +11,23 @@ import (
 )
 
 type FlightRecord struct {
-	CreatedAt       string `json:"create_at"`
-	CreatedStr      string `json:"create_str"`
+	CreatedAt       string `json:"created_at"`
+	CreatedStr      string `json:"created_str"`
 	FlightMissionId string `json:"flight_mission_id"`
 	Id              string `json:"id"`
 	LocalId         string `json:"local_id"`
 	Message         string `json:"message"`
 	RecordAt        string `json:"record_at"`
 }
-
+func (fr FlightRecord)String()string{
+	return fmt.Sprintf(`{CreatedAt:"%v" CreatedStr:"%v" FlightMissionId:"%v" Id:"%v" LocalId:"%v" Message:"%v" RecordAt:"%v"}`,fr.CreatedAt,fr.CreatedStr,fr.FlightMissionId,fr.Id,fr.LocalId,fr.Message,fr.RecordAt)
+}
 func main() {
-	data := ParseData(ReadFile("test.json"))
+	data := ParseData(ReadFile("624670.json"))
 	max := int64(-1)
 	min := int64(-1)
 	for _, v := range data {
+		//fmt.Println(v)
 		tmp := ParseTimestamp(v.CreatedStr)
 		if max == -1 {
 			max = tmp
